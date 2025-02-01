0d04bfe67484698e1e8e682041a7bc905584e9bc
Beinsezii <beinsezii@gmail.com>
2025-01-31 21:03:52 -0800

    Rough flux pipe

--- skrample/diffusers.py
+++ skrample/diffusers.py
@@ -1,12 +1,49 @@
+import collections
+import math
+
+import numpy as np
 import torch
 from torch import Tensor
-import numpy as np
 
 
 class SkrampleScheduler:
     _timesteps: Tensor = torch.empty([0], dtype=torch.float64)
     _sigmas: Tensor = torch.empty([0], dtype=torch.float64)
     _device: torch.device = torch.device("cpu")
+    flow: bool  # Hack for before we split
+
+    def __init__(self, flow: bool = False):
+        self.flow = flow
+        self.set_timesteps(1000)
+
+    @property
+    def timesteps(self) -> Tensor:
+        return self._timesteps.clone().to(self._device)
+
+    @property
+    def sigmas(self) -> Tensor:
+        return self._sigmas.clone().to(self._device)
+
+    @property
+    def order(self) -> int:
+        return 1
+
+    @property
+    def config(self):
+        # Flux
+        fake_config = {
+            "base_image_seq_len": 256,
+            "base_shift": 0.5,
+            "max_image_seq_len": 4096,
+            "max_shift": 1.15,
+            "num_train_timesteps": 1000,
+            "shift": 3.0,
+            "use_dynamic_shifting": True,
+        }
+        return collections.namedtuple("FrozenDict", field_names=fake_config.keys())(**fake_config)
+
+    def time_shift(self, mu: float, sigma: float, t: Tensor):
+        return math.exp(mu) / (math.exp(mu) + (1 / t - 1) ** sigma)
 
     def set_timesteps(
         self,
@@ -14,6 +51,7 @@ class SkrampleScheduler:
         device: torch.device | str | None = None,
         timesteps: Tensor | list[int] | None = None,
         sigmas: Tensor | list[float] | None = None,
+        mu: float | None = None,
     ):
         if num_inference_steps is None:
             if timesteps is not None:
@@ -31,47 +69,53 @@ class SkrampleScheduler:
         beta_start = 0.00085
         beta_end = 0.012
 
-        ratio = num_train_timesteps / num_inference_steps
+        shift = 3.0  # Flux
 
-        # # https://arxiv.org/abs/2305.08891 Table 2
-        # # Linspace
-        # new_timesteps = np.linspace(0, num_train_timesteps - 1, num_inference_steps, dtype=np.float64)[::-1].copy()
-        # # Leading
-        new_timesteps = (np.arange(0, num_inference_steps) * ratio).round()[::-1].copy()
-        # # Trailing
-        # new_timesteps = np.arange(num_train_timesteps, 0, -ratio).round().copy() - 1
+        if self.flow:
+            sigmas = torch.linspace(1, 0, num_inference_steps + 1, dtype=torch.float32)[:-1]
 
-        # Step offset for SD
-        new_timesteps += 1
+            if mu is not None:  # dynamic
+                sigmas = self.time_shift(mu, 1.0, sigmas)
+            else:  # non-dynamic
+                sigmas = shift * sigmas / (1 + (shift - 1) * sigmas)
 
-        # Default scaled sigma schedule
-        betas = torch.linspace(beta_start**0.5, beta_end**0.5, num_train_timesteps, dtype=torch.float64) ** 2
-        alphas = 1 - betas
-        alphas_cumprod = torch.cumprod(alphas, dim=0)
-        sigmas = ((1 - alphas_cumprod) / alphas_cumprod) ** 0.5
-        sigmas_np = np.interp(new_timesteps, np.arange(0, len(sigmas)), sigmas.numpy())
-        sigmas = torch.cat([torch.from_numpy(sigmas_np), torch.zeros(1, device=sigmas.device)])
+            new_timesteps = (sigmas * num_train_timesteps).numpy()
+            sigmas = torch.cat([sigmas, torch.zeros(1, device=sigmas.device)])
+
+        else:
+            ratio = num_train_timesteps / num_inference_steps
+
+            # # https://arxiv.org/abs/2305.08891 Table 2
+            # # Linspace
+            # new_timesteps = np.linspace(0, num_train_timesteps - 1, num_inference_steps, dtype=np.float64)[::-1].copy()
+            # # Leading
+            new_timesteps = (np.arange(0, num_inference_steps) * ratio).round()[::-1].copy()
+            # # Trailing
+            # new_timesteps = np.arange(num_train_timesteps, 0, -ratio).round().copy() - 1
+
+            # Step offset for SD
+            new_timesteps += 1
+
+            # Default scaled sigma schedule
+            betas = torch.linspace(beta_start**0.5, beta_end**0.5, num_train_timesteps, dtype=torch.float64) ** 2
+            alphas = 1 - betas
+            alphas_cumprod = torch.cumprod(alphas, dim=0)
+            sigmas = ((1 - alphas_cumprod) / alphas_cumprod) ** 0.5
+            sigmas_np = np.interp(new_timesteps, np.arange(0, len(sigmas)), sigmas.numpy())
+            sigmas = torch.cat([torch.from_numpy(sigmas_np), torch.zeros(1, device=sigmas.device)])
 
         self._sigmas = sigmas
         self._timesteps = torch.from_numpy(new_timesteps)
 
-    @property
-    def timesteps(self) -> Tensor:
-        return self._timesteps.clone().to(self._device)
-
-    @property
-    def sigmas(self) -> Tensor:
-        return self._sigmas.clone().to(self._device)
-
-    @property
-    def order(self) -> int:
-        return 1
-
     def add_noise(self, original_samples: Tensor, noise: Tensor, timesteps: Tensor) -> Tensor:
         step = self._timesteps.tolist().index(timesteps[0].item())
         sigma = self._sigmas[step]
-        print("S1a", sigma)
         return original_samples + noise * sigma
+
+    def scale_noise(self, sample: Tensor, timestep: Tensor, noise: Tensor) -> Tensor:
+        step = self._timesteps.tolist().index(timestep.item())
+        sigma = self._sigmas[step]
+        return sigma * noise + (1.0 - sigma) * sample
 
     def step(
         self,
@@ -91,17 +135,22 @@ class SkrampleScheduler:
         sigma = self._sigmas[step]
         sigma_next = self._sigmas[step + 1]
 
-        # # Sample
-        # pred_original_sample = model_output
-        # Epsilon
-        pred_original_sample = sample.to(torch.float32) - sigma * model_output.to(torch.float32)
-        # # denoised = model_output * c_out + input * c_skip
-        # # Velocity
-        # pred_original_sample = model_output * (-sigma / (sigma**2 + 1) ** 0.5) + (sample / (sigma**2 + 1))
+        if self.flow:
+            # Euler Flow
+            prev_sample = sample.to(torch.float32) + (sigma_next - sigma) * model_output.to(torch.float32)
+            pred_original_sample = sample.clone().to(torch.float32)
+        else:
+            # # Sample
+            # pred_original_sample = model_output
+            # Epsilon
+            pred_original_sample = sample.to(torch.float32) - sigma * model_output.to(torch.float32)
+            # # denoised = model_output * c_out + input * c_skip
+            # # Velocity
+            # pred_original_sample = model_output * (-sigma / (sigma**2 + 1) ** 0.5) + (sample / (sigma**2 + 1))
 
-        # Euler
-        derivative = (sample - pred_original_sample) / sigma
-        prev_sample = sample + derivative * (sigma_next - sigma)
+            # Euler
+            derivative = (sample - pred_original_sample) / sigma
+            prev_sample = sample + derivative * (sigma_next - sigma)
 
         if return_dict:
             raise ValueError
@@ -114,5 +163,4 @@ class SkrampleScheduler:
     def scale_model_input(self, sample: Tensor, timestep: float | Tensor) -> Tensor:
         step = self._timesteps.tolist().index(timestep if isinstance(timestep, (int | float)) else timestep.item())
         sigma = self._sigmas[step]
-        print("S1i", sigma)
         return sample / ((sigma**2 + 1) ** 0.5)
--- tests/diffusers_pipes.py
+++ tests/diffusers_pipes.py
@@ -1,8 +1,12 @@
+import gc
+
 import torch
+from diffusers.pipelines.flux.pipeline_flux_img2img import FluxImg2ImgPipeline
 from diffusers.pipelines.stable_diffusion_xl.pipeline_stable_diffusion_xl_img2img import (
     StableDiffusionXLImg2ImgPipeline,
 )
 from diffusers.schedulers.scheduling_euler_discrete import EulerDiscreteScheduler
+from diffusers.schedulers.scheduling_flow_match_euler_discrete import FlowMatchEulerDiscreteScheduler
 
 from skrample.diffusers import SkrampleScheduler
 
@@ -14,9 +18,9 @@ def compare_latents(a: torch.Tensor, b: torch.Tensor, margin: float = 1e-8):
 
 
 def compare_schedulers(
-    pipe: StableDiffusionXLImg2ImgPipeline,
+    pipe: StableDiffusionXLImg2ImgPipeline | FluxImg2ImgPipeline,
     a: SkrampleScheduler,
-    b: EulerDiscreteScheduler,
+    b: EulerDiscreteScheduler | FlowMatchEulerDiscreteScheduler,
     margin: float = 1e-8,
     **kwargs,
 ):
@@ -38,11 +42,12 @@ def compare_schedulers(
     compare_latents(a_o, b_o, margin=margin)
 
 
+@torch.inference_mode()
 def test_sdxl_i2i():
-    dt, dv = torch.float32, torch.device("cpu")
-    pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
-        "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=dt
-    ).to(dv)
+    gc.collect()
+    dt, dv = torch.float32, torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=dt)
+    pipe.enable_model_cpu_offload(device=dv)
     assert isinstance(pipe, StableDiffusionXLImg2ImgPipeline)
 
     a = SkrampleScheduler()
@@ -60,3 +65,36 @@ def test_sdxl_i2i():
         pooled_prompt_embeds=torch.zeros([1, 1280], dtype=dt, device=dv),
         negative_pooled_prompt_embeds=torch.zeros([1, 1280], dtype=dt, device=dv),
     )
+
+
+@torch.inference_mode()
+def test_flux_i2i():
+    gc.collect()
+    dt, dv = torch.float16, torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    pipe = FluxImg2ImgPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=dt)
+    pipe.enable_model_cpu_offload(device=dv)
+    assert isinstance(pipe, FluxImg2ImgPipeline)
+
+    a = SkrampleScheduler(flow=True)
+    b = FlowMatchEulerDiscreteScheduler.from_config(pipe.scheduler.config)
+    assert isinstance(b, FlowMatchEulerDiscreteScheduler)
+
+    fn, pipe._encode_vae_image = (
+        pipe._encode_vae_image,
+        lambda *_, **__: torch.zeros([1, 16, 32, 32], dtype=dt, device=dv),
+    )
+
+    compare_schedulers(
+        pipe,
+        a,
+        b,
+        5e-3,  # close enough for now
+        height=256,
+        width=256,
+        image=torch.zeros([1, 1, 1], dtype=dt, device=dv),
+        num_inference_steps=8,
+        prompt_embeds=torch.zeros([1, 512, 4096], dtype=dt, device=dv),
+        pooled_prompt_embeds=torch.zeros([1, 768], dtype=dt, device=dv),
+    )
+
+    pipe._encode_vae_image = fn
