adc9f31728d7c62f7b50a96a977046fbf84c6db5
Beinsezii <beinsezii@gmail.com>
2025-02-02 15:27:10 -0800

    Add Flow direct schedule test

--- tests/diffusers_schedules.py
+++ tests/diffusers_schedules.py
@@ -1,21 +1,36 @@
 import torch
 from diffusers.schedulers.scheduling_euler_discrete import EulerDiscreteScheduler
+from diffusers.schedulers.scheduling_flow_match_euler_discrete import FlowMatchEulerDiscreteScheduler
 
-from skrample.scheduling import Scaled, SkrampleSchedule
+from skrample.scheduling import Flow, Scaled, SkrampleSchedule
 from tests.common import compare_tensors, hf_scheduler_config
 
 
 def compare_schedules(
     a: SkrampleSchedule,
-    b: EulerDiscreteScheduler,
+    b: EulerDiscreteScheduler | FlowMatchEulerDiscreteScheduler,
+    mu: float | None = None,
     ts_margin: float = 1.0,
     sig_margin: float = 1e-3,
 ):
     for steps in range(1, 12):
-        b.set_timesteps(num_inference_steps=steps)
+        if isinstance(b, FlowMatchEulerDiscreteScheduler):
+            b.set_timesteps(num_inference_steps=steps, mu=mu)
+        else:
+            b.set_timesteps(num_inference_steps=steps)
 
-        compare_tensors(torch.from_numpy(a.timesteps(steps)), b.timesteps, f"TIMESTEPS @ {steps}", margin=ts_margin)
-        compare_tensors(torch.from_numpy(a.sigmas(steps)), b.sigmas[:-1], f"SIGMAS @ {steps}", margin=sig_margin)
+        compare_tensors(
+            torch.from_numpy(a.timesteps(steps, mu=mu)),
+            b.timesteps,
+            f"TIMESTEPS @ {steps}",
+            margin=ts_margin,
+        )
+        compare_tensors(
+            torch.from_numpy(a.sigmas(steps, mu=mu)),
+            b.sigmas[:-1],  # type: ignore  # FloatTensor
+            f"SIGMAS @ {steps}",
+            margin=sig_margin,
+        )
 
 
 def test_scaled():
@@ -34,4 +49,24 @@ def test_scaled_uniform():
             hf_scheduler_config("stabilityai/stable-diffusion-xl-base-1.0"),
             timestep_spacing="trailing",
         ),
+    )
+
+
+def test_flow_dynamic():
+    compare_schedules(
+        Flow(),
+        FlowMatchEulerDiscreteScheduler.from_config(  # type: ignore  # Diffusers return BS
+            hf_scheduler_config("black-forest-labs/FLUX.1-dev"),
+        ),
+        mu=0.7,
+    )
+
+
+def test_flow():
+    compare_schedules(
+        Flow(),
+        FlowMatchEulerDiscreteScheduler.from_config(  # type: ignore  # Diffusers return BS
+            hf_scheduler_config("stabilityai/stable-diffusion-3-medium-diffusers"),
+        ),
+        mu=None,
     )
