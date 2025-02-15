5e02f5b97b4ac88fa29b08c5e98b74960a703e2d
Beinsezii <beinsezii@gmail.com>
2025-02-14 19:04:32 -0800

    DPM SDE UT

--- tests/diffusers_samplers.py
+++ tests/diffusers_samplers.py
@@ -7,17 +7,21 @@ from testing_common import compare_tensors, hf_scheduler_config
 
 from skrample.sampling import DPM, EPSILON, FLOW, VELOCITY, Euler, SkrampleSampler, SKSamples, UniPC
 
+DiffusersScheduler = (
+    EulerDiscreteScheduler | DPMSolverMultistepScheduler | FlowMatchEulerDiscreteScheduler | UniPCMultistepScheduler
+)
+
 
 def dual_sample(
     a: SkrampleSampler,
-    b: EulerDiscreteScheduler | FlowMatchEulerDiscreteScheduler,
+    b: DiffusersScheduler,
     steps: range,
     mu: float | None = None,
 ) -> tuple[torch.Tensor, torch.Tensor]:
     # Inputs
     a_sample = torch.zeros([1, 4, 128, 128], dtype=torch.float32)
     b_sample = a_sample.clone()
-    noise = torch.randn(a_sample.shape, generator=torch.manual_seed(0), dtype=a_sample.dtype)
+    initial_noise = torch.randn(a_sample.shape, generator=torch.manual_seed(0), dtype=a_sample.dtype)
 
     if isinstance(b, FlowMatchEulerDiscreteScheduler):
         b.set_timesteps(steps.stop, mu=mu)
@@ -29,23 +33,25 @@ def dual_sample(
     timestep, sigma = schedule[steps.start]
 
     if isinstance(b, FlowMatchEulerDiscreteScheduler):
-        b_sample = b.scale_noise(sample=b_sample, timestep=timestep.unsqueeze(0), noise=noise)  # type: ignore  # FloatTensor
+        b_sample = b.scale_noise(sample=b_sample, timestep=timestep.unsqueeze(0), noise=initial_noise)  # type: ignore  # FloatTensor
         subnormal = True
     else:
-        b_sample = b.add_noise(original_samples=b_sample, noise=noise, timesteps=timestep.unsqueeze(0))
+        b_sample = b.add_noise(original_samples=b_sample, noise=initial_noise, timesteps=timestep.unsqueeze(0))  # type: ignore  # IntTensor
         subnormal = False
 
-    a_sample = a.merge_noise(a_sample, noise, sigma.item(), subnormal=subnormal)
+    a_sample = a.merge_noise(a_sample, initial_noise, sigma.item(), subnormal=subnormal)
 
     prior_steps: list[SKSamples] = []
     for step in steps:
         # Just some pseud-random transform that shouldn't blow up the values
         model = torch.randn([128, 128], generator=torch.manual_seed(step), dtype=a_sample.dtype)
 
+        noise = torch.randn(a_sample.shape, generator=torch.manual_seed(step + steps.stop * 2), dtype=a_sample.dtype)
+
         timestep, sigma = schedule[step]
 
         a_output = a.scale_input(a_sample, sigma.item(), subnormal=subnormal) * model
-        sampled = a.sample(a_sample, a_output, schedule[:, 1].numpy(), step, previous=prior_steps, subnormal=subnormal)
+        sampled = a.sample(a_sample, a_output, schedule[:, 1].numpy(), step, noise, prior_steps, subnormal)
         a_sample = sampled.final
         prior_steps.append(sampled)
 
@@ -54,14 +60,14 @@ def dual_sample(
         else:
             b_output = b.scale_model_input(sample=b_sample, timestep=timestep) * model
 
-        b_sample = b.step(model_output=b_output, sample=b_sample, timestep=timestep)[0]  # type: ignore  # FloatTensor
+        b_sample = b.step(model_output=b_output, sample=b_sample, timestep=timestep, variance_noise=noise)[0]  # type: ignore  # FloatTensor
 
     return a_sample, b_sample
 
 
 def compare_samplers(
     a: SkrampleSampler,
-    b: EulerDiscreteScheduler,
+    b: DiffusersScheduler,
     mu: float | None = None,
     margin: float = 1e-8,
     message: str = "",
@@ -99,17 +105,18 @@ def test_euler_flow():
 def test_dpm():
     for predictor in [(EPSILON, "epsilon"), (VELOCITY, "v_prediction"), (FLOW, "flow_prediction")]:
         for order in range(1, 3):
-            compare_samplers(
-                DPM(predictor=predictor[0], order=order),
-                DPMSolverMultistepScheduler.from_config(  # type: ignore  # Diffusers return BS
-                    hf_scheduler_config("stabilityai/stable-diffusion-xl-base-1.0"),
-                    algorithm_type="dpmsolver++",
-                    final_sigmas_type="zero",
-                    solver_order=order,
-                    prediction_type=predictor[1],
-                ),
-                message=f"{predictor[0].__name__} o{order}",
-            )
+            for stochastic in [False, True]:
+                compare_samplers(
+                    DPM(predictor=predictor[0], order=order, add_noise=stochastic),
+                    DPMSolverMultistepScheduler.from_config(  # type: ignore  # Diffusers return BS
+                        hf_scheduler_config("stabilityai/stable-diffusion-xl-base-1.0"),
+                        algorithm_type="sde-dpmsolver++" if stochastic else "dpmsolver++",
+                        final_sigmas_type="zero",
+                        solver_order=order,
+                        prediction_type=predictor[1],
+                    ),
+                    message=f"{predictor[0].__name__} o{order} s{stochastic}",
+                )
 
 
 def test_unipc():
