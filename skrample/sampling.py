2d1d364c23058fa774e91db2e838f27b3eb323b1
Beinsezii <beinsezii@gmail.com>
2025-02-17 19:32:29 -0800

    Deduplicate UniPC

--- skrample/sampling.py
+++ skrample/sampling.py
@@ -300,179 +300,73 @@ class UniPC(HighOrderSampler):
         # 4-6 is mostly stable now, 7-9 depends on the model. What ranges are actually useful..?
         return 9
 
-    # diffusers.sigma_schedulers.scheduling_unipc_multistep.UniPCMultistepScheduler.multistep_uni_c_bh_update
-    def unified_corrector(
+    def _uni_p_c_prelude(
         self,
-        sample: Sample,
         prediction: Sample,
-        sigma_schedule: NDArray,
         step: int,
-        previous: list[SKSamples] = [],
-        subnormal: bool = False,
-    ) -> Sample:
+        sigma_schedule: NDArray,
+        previous: list[SKSamples],
+        subnormal: bool,
+        lambda_X: float,
+        h_X: float,
+        order: int,
+        prior: bool,
+    ) -> tuple[float, Sample, float | Sample, float]:
         import torch  # einsum operates on the full sample, so numpy would involve lots of data movement.
-
-        # -1 step since it effectively corrects the prior step before the next prediction
-        effective_order = self.effective_order(step - 1, sigma_schedule, previous[:-1])
-
-        sigma = self.get_sigma(step, sigma_schedule)
-        sigma_p1 = self.get_sigma(step - 1, sigma_schedule)
-
-        signorm, alpha = sigma_normal(sigma, subnormal)
-        signorm_p1, alpha_p1 = sigma_normal(sigma_p1, subnormal)
-
-        prediction_p1 = previous[-1].prediction
-        sample_p1 = previous[-1].sample
-
-        lambda_ = safe_log(alpha) - safe_log(signorm)
-        lambda_p1 = safe_log(alpha_p1) - safe_log(signorm_p1)
-
-        h = abs(lambda_ - lambda_p1)
-
-        rks: list[float] = []
-        D1s: list[Sample] = []
-        for i in range(1, effective_order):
-            step_pO1 = step - (i + 1)
-            prediction_pO1 = previous[-(i + 1)].prediction
-            sigma_pO1, alpha_pO1 = sigma_normal(self.get_sigma(step_pO1, sigma_schedule), subnormal)
-            lambda_pO1 = safe_log(alpha_pO1) - safe_log(sigma_pO1)
-            rk = (lambda_pO1 - lambda_p1) / h
-            if math.isfinite(rk):  # for subnormal
-                rks.append(rk)
-            else:
-                rks.append(0)  # TODO: proper value?
-
-            D1s.append((prediction_pO1 - prediction_p1) / rk)
-
-        rks.append(1.0)
-
-        R: list[list[float]] = []
-        b: list[float] = []
 
         # hh = -h if self.predict_x0 else h
-        hh = -h
-        h_phi_1 = math.expm1(hh)  # h\phi_1(h) = e^h - 1
-        h_phi_k = h_phi_1 / hh - 1
+        hh_X = -h_X
+        h_phi_1_X = math.expm1(hh_X)  # h\phi_1(h) = e^h - 1
 
-        # # BH1
+        # # bh1
         # B_h = hh
-        # BH2
-        B_h = math.expm1(hh)
-
-        factorial_i = 1
-        for i in range(1, effective_order + 1):
-            R.append([math.pow(v, i - 1) for v in rks])
-            b.append(h_phi_k * factorial_i / B_h)
-            factorial_i *= i + 1
-            h_phi_k = h_phi_k / hh - 1 / factorial_i
-
-        # for order 1, we use a simplified version
-        if effective_order == 1:
-            rhos_c = torch.tensor([0.5], dtype=sample_p1.dtype, device=sample.device)
-        else:
-            rhos_c = torch.linalg.solve(
-                torch.tensor(R, dtype=sample.dtype, device=sample.device),
-                torch.tensor(b, dtype=sample.dtype, device=sample.device),
-            )
-
-        if D1s:
-            corr_res = torch.einsum("k,bkc...->bc...", rhos_c[:-1], torch.stack(D1s, dim=1))
-        else:
-            corr_res = 0
-
-        # if self.predict_x0:
-        x_t_ = signorm / signorm_p1 * sample_p1 - alpha * h_phi_1 * prediction_p1
-        D1_t = prediction - prediction_p1
-        sample = x_t_ - alpha * B_h * (corr_res + rhos_c[-1] * D1_t)
-        # else:
-        #     x_t_ = alpha_t / alpha_s0 * x - sigma_t * h_phi_1 * m0
-        #     D1_t = model_t - m0
-        #     x_t = x_t_ - sigma_t * B_h * (corr_res + rhos_c[-1] * D1_t)
-        return sample
-
-    # diffusers.sigma_schedulers.scheduling_unipc_multistep.UniPCMultistepScheduler.multistep_uni_p_bh_update
-    def unified_predictor(
-        self,
-        sample: Sample,
-        prediction: Sample,
-        sigma_schedule: NDArray,
-        step: int,
-        previous: list[SKSamples] = [],
-        subnormal: bool = False,
-    ) -> Sample:
-        import torch  # einsum operates on the full sample, so numpy would involve lots of data movement.
-
-        effective_order = self.effective_order(step, sigma_schedule, previous)
-
-        sigma = self.get_sigma(step, sigma_schedule)
-        sigma_n1 = self.get_sigma(step + 1, sigma_schedule)
-
-        signorm, alpha = sigma_normal(sigma, subnormal)
-        signorm_n1, alpha_n1 = sigma_normal(sigma_n1, subnormal)
-
-        lambda_n1 = safe_log(alpha_n1) - safe_log(signorm_n1)
-        lambda_ = safe_log(alpha) - safe_log(signorm)
-
-        h = abs(lambda_n1 - lambda_)
+        # bh2
+        B_h = h_phi_1_X
 
         rks: list[float] = []
         D1s: list[Sample] = []
-        for i in range(1, effective_order):
+        for i in range(1 + prior, order + prior):
             step_pO = step - i
             prediction_pO = previous[-i].prediction
             sigma_pO, alpha_pO = sigma_normal(self.get_sigma(step_pO, sigma_schedule), subnormal)
             lambda_pO = safe_log(alpha_pO) - safe_log(sigma_pO)
-            rk = (lambda_pO - lambda_) / h
+            rk = (lambda_pO - lambda_X) / h_X
             if math.isfinite(rk):  # for subnormal
                 rks.append(rk)
             else:
                 rks.append(0)  # TODO: proper value?
             D1s.append((prediction_pO - prediction) / rk)
 
-        rks.append(1.0)
+        if prior:
+            rks.append(1.0)
 
         R: list[list[float]] = []
         b: list[float] = []
 
-        # hh = -h if self.predict_x0 else h
-        hh = -h
-        h_phi_1 = math.expm1(hh)  # h\phi_1(h) = e^h - 1
-        h_phi_k = h_phi_1 / hh - 1
-
-        # # bh1
-        # B_h = hh
-        # bh2
-        B_h = math.expm1(hh)
+        h_phi_k = h_phi_1_X / hh_X - 1
 
         factorial_i = 1
-        for i in range(1, effective_order + 1):
-            R.append([math.pow(v, i - 1) for v in rks[:-1]])
+        for i in range(1, order + 1):
+            R.append([math.pow(v, i - 1) for v in rks])
             b.append(h_phi_k * factorial_i / B_h)
             factorial_i *= i + 1
-            h_phi_k = h_phi_k / hh - 1 / factorial_i
+            h_phi_k = h_phi_k / hh_X - 1 / factorial_i
+
+        if order <= 2 - prior:
+            rhos = torch.tensor([0.5], dtype=prediction.dtype, device=prediction.device)
+        else:
+            i = len(rks)
+            rhos = torch.linalg.solve(
+                torch.tensor(R[:i], dtype=prediction.dtype, device=prediction.device),
+                torch.tensor(b[:i], dtype=prediction.dtype, device=prediction.device),
+            )
 
         if D1s:
-            # for order 2, we use a simplified version
-            if effective_order == 2:
-                rhos_p = torch.tensor([0.5], dtype=sample.dtype, device=sample.device)
-            else:
-                rhos_p = torch.linalg.solve(
-                    torch.tensor(R[:-1], device=sample.device, dtype=sample.dtype),
-                    torch.tensor(b[:-1], device=sample.device, dtype=sample.dtype),
-                )
-
-            pred_res = torch.einsum("k,bkc...->bc...", rhos_p, torch.stack(D1s, dim=1))
+            uni_res = torch.einsum("k,bkc...->bc...", rhos[:-1] if prior else rhos, torch.stack(D1s, dim=1))
         else:
-            pred_res = 0
+            uni_res = 0
 
-        # if self.predict_x0:
-        x_t_ = signorm_n1 / signorm * sample - alpha_n1 * h_phi_1 * prediction
-        x_t = x_t_ - alpha_n1 * B_h * pred_res
-        # else:
-        #     x_t_ = alpha_t / alpha_s0 * x - sigma_t * h_phi_1 * m0
-        #     x_t = x_t_ - sigma_t * B_h * pred_res
-
-        return x_t
+        return B_h, rhos, uni_res, h_phi_1_X
 
     def sample(
         self,
@@ -487,20 +381,55 @@ class UniPC(HighOrderSampler):
         sigma = self.get_sigma(step, sigma_schedule)
         prediction = self.predictor(sample, output, sigma, subnormal)
 
+        sigma = self.get_sigma(step, sigma_schedule)
+        signorm, alpha = sigma_normal(sigma, subnormal)
+        lambda_ = safe_log(alpha) - safe_log(signorm)
+
         if previous:
-            sample = self.unified_corrector(sample, prediction, sigma_schedule, step, previous, subnormal)
+            # -1 step since it effectively corrects the prior step before the next prediction
+            effective_order = self.effective_order(step - 1, sigma_schedule, previous[:-1])
+
+            sigma_p1 = self.get_sigma(step - 1, sigma_schedule)
+            signorm_p1, alpha_p1 = sigma_normal(sigma_p1, subnormal)
+            lambda_p1 = safe_log(alpha_p1) - safe_log(signorm_p1)
+            h_p1 = abs(lambda_ - lambda_p1)
+
+            prediction_p1 = previous[-1].prediction
+            sample_p1 = previous[-1].sample
+
+            B_h_p1, rhos_c, uni_c_res, h_phi_1_p1 = self._uni_p_c_prelude(
+                prediction_p1, step, sigma_schedule, previous, subnormal, lambda_p1, h_p1, effective_order, True
+            )
+
+            # if self.predict_x0:
+            x_t_ = signorm / signorm_p1 * sample_p1 - alpha * h_phi_1_p1 * prediction_p1
+            D1_t = prediction - prediction_p1
+            sample = x_t_ - alpha * B_h_p1 * (uni_c_res + rhos_c[-1] * D1_t)
+            # else:
+            #     x_t_ = alpha_t / alpha_s0 * x - sigma_t * h_phi_1 * m0
+            #     D1_t = model_t - m0
+            #     x_t = x_t_ - sigma_t * B_h * (corr_res + rhos_c[-1] * D1_t)
 
         if self.solver:
             sampled = self.solver.sample(sample, output, sigma_schedule, step, noise, previous, subnormal).final
         else:
-            sampled = self.unified_predictor(
-                sample,
-                prediction,
-                sigma_schedule,
-                step,
-                previous,
-                subnormal,
+            effective_order = self.effective_order(step, sigma_schedule, previous)
+
+            sigma_n1 = self.get_sigma(step + 1, sigma_schedule)
+            signorm_n1, alpha_n1 = sigma_normal(sigma_n1, subnormal)
+            lambda_n1 = safe_log(alpha_n1) - safe_log(signorm_n1)
+            h = abs(lambda_n1 - lambda_)
+
+            B_h, _, uni_p_res, h_phi_1 = self._uni_p_c_prelude(
+                prediction, step, sigma_schedule, previous, subnormal, lambda_, h, effective_order, False
             )
+
+            # if self.predict_x0:
+            x_t_ = signorm_n1 / signorm * sample - alpha_n1 * h_phi_1 * prediction
+            sampled = x_t_ - alpha_n1 * B_h * uni_p_res
+            # else:
+            #     x_t_ = alpha_t / alpha_s0 * x - sigma_t * h_phi_1 * m0
+            #     x_t = x_t_ - sigma_t * B_h * pred_res
 
         return SKSamples(
             final=sampled,
