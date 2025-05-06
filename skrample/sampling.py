00126834beb2f089e7d043683914e01cb02e195c
Beinsezii <beinsezii@gmail.com>
2025-05-05 21:18:48 -0700

    Rename sampled -> final in sampling fns to match SKSamples

--- skrample/sampling.py
+++ skrample/sampling.py
@@ -157,10 +157,10 @@ class Euler(SkrampleSampler):
         # thx Qwen
         term1 = (sample * sigma) / sigma_u
         term2 = (term1 - prediction) * (sigma_next / sigma - 1)
-        sampled = (term1 + term2) * ratio
+        final = (term1 + term2) * ratio
 
         return SKSamples(  # type: ignore
-            final=sampled,
+            final=final,
             prediction=prediction,
             sample=sample,
         )
@@ -209,10 +209,10 @@ class DPM(HighOrderSampler, StochasticSampler):
 
         exp2 = math.expm1(hh)
 
-        sampled = noise_factor + (sigma_u_next / sigma_u * exp1) * sample
+        final = noise_factor + (sigma_u_next / sigma_u * exp1) * sample
 
         # 1st order
-        sampled -= (sigma_v_next * exp2) * prediction
+        final -= (sigma_v_next * exp2) * prediction
 
         effective_order = self.effective_order(step, sigma_schedule, previous)
 
@@ -241,14 +241,14 @@ class DPM(HighOrderSampler, StochasticSampler):
                 D1 = D1_0 + (r / (r + r_prev2)) * (D1_0 - D1_1)
                 D2 = (1.0 / (r + r_prev2)) * (D1_0 - D1_1)
 
-                sampled -= (sigma_v_next * (exp2 / hh - 1.0)) * D1
-                sampled -= (sigma_v_next * ((exp2 - hh) / hh**2 - 0.5)) * D2
+                final -= (sigma_v_next * (exp2 / hh - 1.0)) * D1
+                final -= (sigma_v_next * ((exp2 - hh) / hh**2 - 0.5)) * D2
 
             else:  # 2nd order. using this in O3 produces valid images but not going to risk correctness
-                sampled -= (0.5 * sigma_v_next * exp2) * D1_0
+                final -= (0.5 * sigma_v_next * exp2) * D1_0
 
         return SKSamples(  # type: ignore
-            final=sampled,
+            final=final,
             prediction=prediction,
             sample=sample,
         )
@@ -296,10 +296,10 @@ class Adams(HighOrderSampler):
 
         term1 = (sample * sigma) / sigma_u
         term2 = (term1 - weighted_prediction) * (sigma_next / sigma - 1)
-        sampled = (term1 + term2) * ratio
+        final = (term1 + term2) * ratio
 
         return SKSamples(
-            final=sampled,
+            final=final,
             prediction=prediction,
             sample=sample,
         )  # type: ignore
@@ -430,9 +430,7 @@ class UniPC(HighOrderSampler):
             #     x_t = x_t_ - sigma_t * B_h * (corr_res + rhos_c[-1] * D1_t)
 
         if self.solver:
-            sampled = self.solver.sample(
-                sample, prediction, step, sigma_schedule, sigma_transform, noise, previous
-            ).final
+            final = self.solver.sample(sample, prediction, step, sigma_schedule, sigma_transform, noise, previous).final
         else:
             effective_order = self.effective_order(step, sigma_schedule, previous)
 
@@ -447,13 +445,13 @@ class UniPC(HighOrderSampler):
 
             # if self.predict_x0:
             x_t_ = sigma_u_next / sigma_u * sample - sigma_v_next * h_phi_1 * prediction
-            sampled = x_t_ - sigma_v_next * B_h * uni_p_res
+            final = x_t_ - sigma_v_next * B_h * uni_p_res
             # else:
             #     x_t_ = alpha_t / alpha_s0 * x - sigma_t * h_phi_1 * m0
             #     x_t = x_t_ - sigma_t * B_h * pred_res
 
         return SKSamples(  # type: ignore
-            final=sampled,
+            final=final,
             prediction=prediction,
             sample=sample,
         )
