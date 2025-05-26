52394e60492e6b61667d2b8a7bbbd770dc1f7aa8
Beinsezii <beinsezii@gmail.com>
2025-05-26 02:15:50 -0700

    Refactor UniPC -> UniSolver, UniPC
    UniSolver uses an even more deduplicated solver function along with
    being a standalone SkrampleSampler.
    
    UniPC now simply calls UniSolver for C and optionally P

--- skrample/sampling.py
+++ skrample/sampling.py
@@ -310,13 +310,129 @@ class Adams(HighOrderSampler, Euler):
 
 
 @dataclass(frozen=True)
-class UniPC(HighOrderSampler):
+class UniSolver(HighOrderSampler):
+    "Just the solver from UniPC without any correction stages."
+
+    @staticmethod
+    def max_order() -> int:
+        # TODO(beinsezii): seems more stable after converting to python scalars
+        # 4-6 is mostly stable now, 7-9 depends on the model. What ranges are actually useful..?
+        return 9
+
+    def unisolve[T: Sample](
+        self,
+        sample: T,
+        prediction: T,
+        step: int,
+        sigma_schedule: NDArray,
+        sigma_transform: SigmaTransform,
+        noise: T | None = None,
+        previous: tuple[SKSamples[T], ...] = (),
+        prediction_next: Sample | None = None,
+    ) -> T:
+        "Passing `prediction_next` is equivalent to UniC, otherwise behaves as UniP"
+        sigma = self.get_sigma(step, sigma_schedule)
+
+        sigma = self.get_sigma(step, sigma_schedule)
+        sigma_u, sigma_v = sigma_transform(sigma)
+        lambda_ = safe_log(sigma_v) - safe_log(sigma_u)
+
+        effective_order = self.effective_order(step, sigma_schedule, previous)
+
+        sigma_next = self.get_sigma(step + 1, sigma_schedule)
+        sigma_u_next, sigma_v_next = sigma_transform(sigma_next)
+        lambda_next = safe_log(sigma_v_next) - safe_log(sigma_u_next)
+        h = abs(lambda_next - lambda_)
+
+        # hh = -h if self.predict_x0 else h
+        hh_X = -h
+        h_phi_1 = math.expm1(hh_X)  # h\phi_1(h) = e^h - 1
+
+        # # bh1
+        # B_h = hh
+        # bh2
+        B_h = h_phi_1
+
+        rks: list[float] = []
+        D1s: list[Sample] = []
+        for n in range(1, effective_order):
+            step_prev_N = step - n
+            prediction_prev_N = previous[-n].prediction
+            sigma_u_prev_N, sigma_v_prev_N = sigma_transform(self.get_sigma(step_prev_N, sigma_schedule))
+            lambda_pO = safe_log(sigma_v_prev_N) - safe_log(sigma_u_prev_N)
+            rk = (lambda_pO - lambda_) / h
+            if math.isfinite(rk):  # for subnormal
+                rks.append(rk)
+            else:
+                rks.append(0)  # TODO(beinsezii): proper value?
+            D1s.append((prediction_prev_N - prediction) / rk)
+
+        # WARN(beinsezii): this part feels wrong but it's what diffusers does
+        if prediction_next is not None:
+            rks.append(1.0)
+            order_check: int = 1
+        else:
+            order_check = 2
+
+        R: list[list[float]] = []
+        b: list[float] = []
+
+        h_phi_k = h_phi_1 / hh_X - 1
+
+        for n in range(1, effective_order + 1):
+            R.append([math.pow(v, n - 1) for v in rks])
+            b.append(h_phi_k * math.factorial(n) / B_h)
+            h_phi_k = h_phi_k / hh_X - 1 / math.factorial(n + 1)
+
+        if effective_order <= order_check:
+            rhos: list[float] = [0.5]
+        else:
+            # small array order x order, fast to do it in just np
+            n = len(rks)
+            rhos = np.linalg.solve(R[:n], b[:n]).tolist()
+
+        result = math.sumprod(rhos[: len(D1s)], D1s)  # type: ignore  # Float
+
+        # if self.predict_x0:
+        x_t_ = sigma_u_next / sigma_u * sample - sigma_v_next * h_phi_1 * prediction
+
+        if prediction_next is not None:
+            D1_t = prediction_next - prediction
+            final = x_t_ - sigma_v_next * B_h * (result + rhos[-1] * D1_t)
+        else:
+            final = x_t_ - sigma_v_next * B_h * result
+
+        # else:
+        #     x_t_ = alpha_t / alpha_s0 * x - sigma_t * h_phi_1 * m0
+        #     x_t = x_t_ - sigma_t * B_h * pred_res
+
+        return final  # type: ignore
+
+    def sample[T: Sample](
+        self,
+        sample: T,
+        prediction: T,
+        step: int,
+        sigma_schedule: NDArray,
+        sigma_transform: SigmaTransform,
+        noise: T | None = None,
+        previous: tuple[SKSamples[T], ...] = (),
+    ) -> SKSamples[T]:
+        return SKSamples(  # type: ignore
+            final=self.unisolve(sample, prediction, step, sigma_schedule, sigma_transform, noise, previous),
+            prediction=prediction,
+            sample=sample,
+        )
+
+
+@dataclass(frozen=True)
+class UniPC(UniSolver):
     """Unique sampler that can correct other samplers or its own prediction function.
-    The additional correction essentially adds +1 order on top of what is set."""
+    The additional correction essentially adds +1 order on top of what is set.
+    https://arxiv.org/abs/2302.04867"""
 
     solver: SkrampleSampler | None = None
-    """If set, will use another sampler then perform its own correction.
-    May break, particularly if the solver uses different scaling for noise or input."""
+    "If not set, defaults to `UniSolver(order=self.order)`"
 
     @staticmethod
     def max_order() -> int:
@@ -333,66 +449,6 @@ class UniPC(HighOrderSampler):
         # +1 for correction
         return max(super().require_previous + 1, self.solver.require_previous if self.solver else 0)
 
-    def _uni_p_c_prelude[T: Sample](
-        self,
-        prediction: T,
-        step: int,
-        sigma_schedule: NDArray,
-        sigma_transform: SigmaTransform,
-        previous: tuple[SKSamples[T], ...],
-        lambda_X: float,
-        h_X: float,
-        order: int,
-        prior: bool,
-    ) -> tuple[float, list[float], float | T, float]:
-        "B_h, rhos, result, h_phi_1_X"
-        # hh = -h if self.predict_x0 else h
-        hh_X = -h_X
-        h_phi_1_X = math.expm1(hh_X)  # h\phi_1(h) = e^h - 1
-
-        # # bh1
-        # B_h = hh
-        # bh2
-        B_h = h_phi_1_X
-
-        rks: list[float] = []
-        D1s: list[Sample] = []
-        for n in range(1 + prior, order + prior):
-            step_prev_N = step - n
-            prediction_prev_N = previous[-n].prediction
-            sigma_u_prev_N, sigma_v_prev_N = sigma_transform(self.get_sigma(step_prev_N, sigma_schedule))
-            lambda_pO = safe_log(sigma_v_prev_N) - safe_log(sigma_u_prev_N)
-            rk = (lambda_pO - lambda_X) / h_X
-            if math.isfinite(rk):  # for subnormal
-                rks.append(rk)
-            else:
-                rks.append(0)  # TODO(beinsezii): proper value?
-            D1s.append((prediction_prev_N - prediction) / rk)
-
-        if prior:
-            rks.append(1.0)
-
-        R: list[list[float]] = []
-        b: list[float] = []
-
-        h_phi_k = h_phi_1_X / hh_X - 1
-
-        for n in range(1, order + 1):
-            R.append([math.pow(v, n - 1) for v in rks])
-            b.append(h_phi_k * math.factorial(n) / B_h)
-            h_phi_k = h_phi_k / hh_X - 1 / math.factorial(n + 1)
-
-        if order <= 2 - prior:
-            rhos: list[float] = [0.5]
-        else:
-            # small array order x order, fast to do it in just np
-            n = len(rks)
-            rhos = np.linalg.solve(R[:n], b[:n]).tolist()  # type: ignore
-
-        uni_res = math.sumprod(rhos[: len(D1s)], D1s)  # type: ignore  # Float
-
-        return B_h, rhos, uni_res, h_phi_1_X
-
     def sample[T: Sample](
         self,
         sample: T,
@@ -403,70 +459,26 @@ class UniPC(HighOrderSampler):
         noise: T | None = None,
         previous: tuple[SKSamples[T], ...] = (),
     ) -> SKSamples[T]:
-        sigma = self.get_sigma(step, sigma_schedule)
-
-        sigma = self.get_sigma(step, sigma_schedule)
-        sigma_u, sigma_v = sigma_transform(sigma)
-        lambda_ = safe_log(sigma_v) - safe_log(sigma_u)
-
         if previous:
-            # -1 step since it effectively corrects the prior step before the next prediction
-            effective_order = self.effective_order(step - 1, sigma_schedule, previous[:-1])
-
-            sigma_prev = self.get_sigma(step - 1, sigma_schedule)
-            sigma_u_prev, sigma_v_prev = sigma_transform(sigma_prev)
-            lambda_prev = safe_log(sigma_v_prev) - safe_log(sigma_u_prev)
-            h_prev = abs(lambda_ - lambda_prev)
-
-            prediction_prev = previous[-1].prediction
-            sample_prev = previous[-1].sample
-
-            B_h_prev, rhos_c, uni_c_res, h_phi_1_prev = self._uni_p_c_prelude(
-                prediction_prev,
-                step,
+            sample = self.unisolve(
+                previous[-1].sample,
+                previous[-1].prediction,
+                step - 1,
                 sigma_schedule,
                 sigma_transform,
-                previous,
-                lambda_prev,
-                h_prev,
-                effective_order,
-                True,
+                noise,
+                previous[:-1],
+                prediction_next=prediction,
             )
 
-            # if self.predict_x0:
-            x_t_ = sigma_u / sigma_u_prev * sample_prev - sigma_v * h_phi_1_prev * prediction_prev
-            D1_t = prediction - prediction_prev
-            sample = x_t_ - sigma_v * B_h_prev * (uni_c_res + rhos_c[-1] * D1_t)  # type: ignore
-            # else:
-            #     x_t_ = alpha_t / alpha_s0 * x - sigma_t * h_phi_1 * m0
-            #     D1_t = model_t - m0
-            #     x_t = x_t_ - sigma_t * B_h * (corr_res + rhos_c[-1] * D1_t)
-
-        if self.solver:
-            final = self.solver.sample(sample, prediction, step, sigma_schedule, sigma_transform, noise, previous).final
-        else:
-            effective_order = self.effective_order(step, sigma_schedule, previous)
-
-            sigma_next = self.get_sigma(step + 1, sigma_schedule)
-            sigma_u_next, sigma_v_next = sigma_transform(sigma_next)
-            lambda_next = safe_log(sigma_v_next) - safe_log(sigma_u_next)
-            h = abs(lambda_next - lambda_)
-
-            B_h, _, uni_p_res, h_phi_1 = self._uni_p_c_prelude(
-                prediction, step, sigma_schedule, sigma_transform, previous, lambda_, h, effective_order, False
-            )
-
-            # if self.predict_x0:
-            x_t_ = sigma_u_next / sigma_u * sample - sigma_v_next * h_phi_1 * prediction
-            final = x_t_ - sigma_v_next * B_h * uni_p_res
-            # else:
-            #     x_t_ = alpha_t / alpha_s0 * x - sigma_t * h_phi_1 * m0
-            #     x_t = x_t_ - sigma_t * B_h * pred_res
-
-        return SKSamples(  # type: ignore
-            final=final,
-            prediction=prediction,
-            sample=sample,
+        return (self.solver if self.solver else super()).sample(
+            sample,
+            prediction,
+            step,
+            sigma_schedule,
+            sigma_transform,
+            noise,
+            previous,
         )
 
 
