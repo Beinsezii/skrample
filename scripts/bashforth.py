2a49cd783d17e1f79dda0276a87c5edb1d1d4091
Beinsezii <beinsezii@gmail.com>
2025-06-14 01:11:19 -0700

    Add T201 to scripts/ per file ignore

--- scripts/bashforth.py
+++ scripts/bashforth.py
@@ -11,4 +11,4 @@ def bashforth(order: int) -> list[sp.Rational]:
     return [sp.Rational(a_i) for a_i in a]
 
 
-print(tuple(tuple(bashforth(o)) for o in range(1, 6)))  # noqa: T201
+print(tuple(tuple(bashforth(o)) for o in range(1, 6)))
