5fa263583bd394b87f6dcb203b67d24f2776f581
Beinsezii <beinsezii@gmail.com>
2026-03-07 05:07:44 -0800

    fix i2i

--- skrample/diffusers.py
+++ skrample/diffusers.py
@@ -595,6 +595,7 @@ class RKUltraWrapperScheduler:
         return math.exp(mu) / (math.exp(mu) + (1 / t - 1) ** sigma)
 
     def set_begin_index(self, begin_index: int = 0) -> None:
+        self._index = begin_index
         self.fake_config["begin_index"] = begin_index
 
     def set_timesteps(
